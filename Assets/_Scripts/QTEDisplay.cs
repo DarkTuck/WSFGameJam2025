27430c0a03e7ce037368257223026324a1626a8e
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2025-11-08 05:25:07 +0100

    QTE react to input [half-done] -Arrows spawn correctly -Arrows change color:     -when current     -when done correctly

--- Assets/_Scripts/QTEDisplay.cs
+++ Assets/_Scripts/QTEDisplay.cs
@@ -6,7 +6,10 @@ namespace _Scripts
 {
     public class QTEDisplay : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI text;
+        //[SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private float gridOffset;
+        [SerializeField] private GameObject prefab;
+        private TextMeshProUGUI[] text;
         private Dictionary<Vector2, string> displayDirections = new Dictionary<Vector2, string>
         {
             { Vector2.up, "↑" },
@@ -14,24 +17,41 @@ namespace _Scripts
             { Vector2.left, "←" },
             { Vector2.right, "→" }
         };
-
+        private RectTransform _rectTransform;
+        void Start()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
         public void WriteCharacters(Vector2[] characters)
         {
-            text.text = "";
-            foreach (Vector2 character in characters)
+            Vector2 origin = transform.position;
+            text = new TextMeshProUGUI[characters.Length];
+            for (int i = 0; i < text.Length; i++)
             {
-                text.text += displayDirections[character];
+                Vector2 spawnPosition = new Vector2(i*gridOffset, 0)+origin;
+                SpawnObject(i);
+                text[i].text = displayDirections[characters[i]];
             }
+        }
+
+        void SpawnObject(int id)
+        {
+            var go = Instantiate(prefab,_rectTransform);
+            RectTransform goTransform = go.GetComponent<RectTransform>();
+            goTransform.anchoredPosition = new Vector2((gridOffset*id), 0);
+            go.name = id.ToString();
+            text[id] = go.GetComponent<TextMeshProUGUI>();
+
         }
 
         public void CurrentCharacter(int id)
         {
-            //int meshIndex = text.textInfo.characterInfo[id].materialReferenceIndex;
+            text[id].color = Color.gold;
         }
 
         public void SuccessCharacter(int id)
         {
-           // text.textInfo.characterInfo[id].color = Color.green;
+           text[id].color = Color.green;
         }
         
     }
